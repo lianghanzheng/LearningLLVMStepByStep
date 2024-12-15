67c375e24ea2b70ef93a6d52649df1448b3368d1
ReRoozen <915625352@qq.com>
2024-12-15 14:22:47 +0800

    Bug fix

--- lib/Codegen.cc
+++ lib/Codegen.cc
@@ -12,7 +12,6 @@
 #include "llvm/IR/Verifier.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/raw_ostream.h"
-#include <asm-generic/errno.h>
 #include <memory>
 
 #define DEBUG_TYPE "CodeGen"
